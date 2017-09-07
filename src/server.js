150c27757978fdc6480e29273cd7c57650714480
James Taggart <jxt1607@g.rit.edu>
2017-09-07 12:48:08 -0400

    Update Server to have all possible requests, all htmlHandler responses work but mediaHandler isn't giving anything back to the webpage still

--- src/server.js
+++ src/server.js
@@ -8,14 +8,23 @@ const onRequest = (request, response) => {
   console.dir(request.url);
 
   switch (request.url) {
-    case '/':
+    case '/'||'/index':
       htmlHandler.getIndex(request, response);
       break;
     case '/page2':
-      mediaHandler.getBling(request, response);
+      htmlHandler.getPage2(request, response);
       break;
     case '/page3':
+      htmlHandler.getPage3(request, response);
+      break;
+    case '/bird.mp4':
       mediaHandler.getBird(request, response);
+      break;
+    case '/party.mp4':
+      mediaHandler.getParty(request, response);
+      break;
+    case '/bling.mp3':
+      mediaHandler.getBling(request, response);
       break;
     default:
       htmlHandler.getIndex(request, response);
